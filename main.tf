86e477a8108e9acbeb2f936f53f056497b1b5cd1
Felix Heinonen <fheinonen@gmail.com>
2019-04-02 11:06:15 +0300

    Added packer and ansible scripts

--- main.tf
+++ main.tf
@@ -1,40 +1,76 @@
-# Set the variable value in *.tfvars file
-# or using -var="hcloud_token=..." CLI option
-
-
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
   token = "${var.hcloud_token}"
 }
 
-# Configure AWS provider
-provider "aws" {
-   region = "${var.region}"
-}
-
-#  Main ssh key
 data "hcloud_ssh_key" "ssh_key_1" {
   name = "Hetzner-key"
 }
-# Create 2 new streaming servers. 1 in Nuremberg and 1 in Falkenstein
-resource "hcloud_server" "falkenstein" {
-    name = "falkenstein"
-    location = "fsn1"
-    image = "${var.image}"
-    server_type = "${var.server_type}"
-    ssh_keys = ["${data.hcloud_ssh_key.ssh_key_1.id}"]
-}
-resource "hcloud_server" "nuremberg" {
-    name = "nuremberg"
-    location = "nbg1"
-    image = "${var.image}"
-    server_type = "${var.server_type}"
-    ssh_keys = ["${data.hcloud_ssh_key.ssh_key_1.id}"]
+
+# Configure AWS provider
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region = "${var.region}"
 }
 
-# Create health checks
-resource "aws_route53_health_check" "nuremberg_health" {
-  ip_address        = "${hcloud_server.nuremberg.ipv4_address}"
+# Create new streaming server(s)
+resource "hcloud_server" "video" {
+    count = "${var.server_count}"
+    name = "${element(var.hostname, count.index)}${count.index + 1}-${element(var.location, count.index)}"
+    location = "${element(var.location, count.index)}"
+    image = "${var.image}"
+    server_type = "${var.server_type}"
+    ssh_keys = ["${data.hcloud_ssh_key.ssh_key_1.id}"]
+}
+# Create multianswer A record(s) for server(s)
+resource "aws_route53_record" "tradefox" {
+  count = "${var.server_count}"
+  zone_id = "/hostedzone/Z15ZD56PW5R6X3"
+  name = "tradefox.biz"
+  type    = "A"
+  ttl     = "60"
+  multivalue_answer_routing_policy  = "true"
+
+  set_identifier = "${element(var.hostname, count.index)}${count.index + 1}-${element(var.location, count.index)}"
+  records        = ["${element(hcloud_server.video.*.ipv4_address, count.index)}"]
+  health_check_id = "${element(aws_route53_health_check.health_check.*.id, count.index)}"
+}
+# # Create multianswer AAAA record(s) for server(s)
+resource "aws_route53_record" "tradefoxv6" {
+  count = "${var.server_count}"
+  zone_id = "/hostedzone/Z15ZD56PW5R6X3"
+  name = "tradefox.biz"
+  type    = "AAAA"
+  ttl     = "${var.A_record_TTL}"
+  multivalue_answer_routing_policy  = "true"
+
+  set_identifier = "${element(var.hostname, count.index)}${count.index + 1}-${element(var.location, count.index)}V6"
+  records        = ["${element(hcloud_server.video.*.ipv6_address, count.index)}1"]
+  health_check_id = "${element(aws_route53_health_check.health_check.*.id, count.index)}"
+}
+# Create unique A record(s) for server(s)
+resource "aws_route53_record" "push" {
+  count = "${var.server_count}"
+  zone_id = "/hostedzone/Z15ZD56PW5R6X3"
+  name    = "${element(var.hostname, count.index)}${count.index + 1}-${element(var.location, count.index)}.tradefox.biz."
+  type    = "A"
+  ttl     = "${var.A_record_TTL}"
+  records = ["${element(hcloud_server.video.*.ipv4_address, count.index)}"]
+}
+# Create unique AAAA record(s) for server(s)
+resource "aws_route53_record" "pushv6" {
+  count = "${var.server_count}"
+  zone_id = "/hostedzone/Z15ZD56PW5R6X3"
+  name    = "${element(var.hostname, count.index)}${count.index + 1}-${element(var.location, count.index)}.tradefox.biz."
+  type    = "AAAA"
+  ttl     = "${var.A_record_TTL}"
+  records = ["${element(hcloud_server.video.*.ipv6_address, count.index)}1"]
+}
+# Create health check(s)
+resource "aws_route53_health_check" "health_check" {
+  count = "${var.server_count}"
+  ip_address        = "${element(hcloud_server.video.*.ipv4_address, count.index)}"
   port              = 80
   type              = "HTTP"
   resource_path     = "/live/livestream.m3u8"
@@ -42,74 +78,14 @@ resource "aws_route53_health_check" "nuremberg_health" {
   request_interval  = "10"
 
   tags = {
-    Name = "${hcloud_server.nuremberg.name}"
+    Name = "HealthCheck-${element(var.hostname, count.index)}${count.index + 1}-${element(var.location, count.index)}"
   }
 }
-
-resource "aws_route53_health_check" "falkenstein_health" {
-  ip_address        = "${hcloud_server.falkenstein.ipv4_address}"
-  port              = 80
-  type              = "HTTP"
-  resource_path     = "/live/livestream.m3u8"
-  failure_threshold = "3"
-  request_interval  = "10"
-
-  tags = {
-    Name = "${hcloud_server.falkenstein.name}"
-  }
-}
-
-# Create A records for DNS weighting
-resource "aws_route53_record" "tradefox_nuremberg" {
-  zone_id = "your_zone_id"
-  name    = "your_name"
-  type    = "A"
-  ttl     = "60"
-
-  weighted_routing_policy {
-    weight = 1
-  }
-
-  set_identifier = "${hcloud_server.nuremberg.name}"
-  records        = ["${hcloud_server.nuremberg.ipv4_address}"]
-  health_check_id = "${aws_route53_health_check.nuremberg_health.id}"
-}
-resource "aws_route53_record" "tradefox_falkenstein" {
-  zone_id = "your_zone_id"
-  name    = "your_name"
-  type    = "A"
-  ttl     = "60"
-
-  weighted_routing_policy {
-    weight = 1
-  }
-
-  set_identifier = "${hcloud_server.falkenstein.name}"
-  records        = ["${hcloud_server.falkenstein.ipv4_address}"]
-  health_check_id = "${aws_route53_health_check.falkenstein_health.id}"
-}
-
-# DNS records for pushing streams
-resource "aws_route53_record" "nuremberg_push" {
-  zone_id = "your_zone_id"
-  name    = "your_name"
-  type    = "A"
-  ttl     = "60"
-  records = ["${hcloud_server.nuremberg.ipv4_address}"]
-}
-
-resource "aws_route53_record" "falkenstein_push" {
-  zone_id = "your_zone_id"
-  name    = "your_name"
-  type    = "A"
-  ttl     = "60"
-  records = ["${hcloud_server.falkenstein.ipv4_address}"]
-}
-
-# Create Alarms
-resource "aws_cloudwatch_metric_alarm" "alarm_nuremberg" {
-  alarm_name = "${hcloud_server.nuremberg.name}"
-  alarm_actions = ["your_sns_topic"]
+# Create alarm(s) for server(s)
+resource "aws_cloudwatch_metric_alarm" "alarm" {
+  count = "${var.server_count}"
+  alarm_name = "Alarm-${element(var.hostname, count.index)}${count.index + 1}-${element(var.location, count.index)}"
+  alarm_actions = ["arn:aws:sns:us-east-1:991445153020:Nuremberg1_is_down"]
   comparison_operator = "LessThanThreshold"
   ok_actions = []
   evaluation_periods = "1"
@@ -123,26 +99,6 @@ resource "aws_cloudwatch_metric_alarm" "alarm_nuremberg" {
   insufficient_data_actions = []
   actions_enabled = "true"
   dimensions = {
-      HealthCheckId = "${aws_route53_health_check.nuremberg_health.id}"
-    }
-  }
-
-  resource "aws_cloudwatch_metric_alarm" "alarm_falkenstein" {
-  alarm_name = "${hcloud_server.falkenstein.name}"
-  alarm_actions = ["your_sns_topic"]
-  comparison_operator = "LessThanThreshold"
-  ok_actions = []
-  evaluation_periods = "1"
-  metric_name = "HealthCheckStatus"
-  namespace = "AWS/Route53"
-  period = "60"
-  statistic = "Minimum"
-  threshold = "1.0"
-  evaluation_periods = "1"
-  alarm_description = "This metric monitors streaming server falkenstein livestream"
-  insufficient_data_actions = []
-  actions_enabled = "true"
-  dimensions = {
-      HealthCheckId = "${aws_route53_health_check.falkenstein_health.id}"
+      HealthCheckId = "${element(aws_route53_health_check.health_check.*.id, count.index)}"
     }
   }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,15 @@ variable "hcloud_token" {}
 variable "image" {}
 variable "server_type" {}
 variable "region" {}
+variable "hostname" {default = 
+    ["test"]}
+variable "server_count" {}
+variable "location" {
+    default = 
+    ["nbg1", "fsn1"]
+    }
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "A_record_TTL" {}
