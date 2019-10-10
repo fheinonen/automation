e8af64afb2ccfd1111433ac89faa4521e37477d6
Felix Heinonen <fheinonen@gmail.com>
2019-10-10 09:20:07 +0300

    terraform 0.12 compliant code

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
-  token = "${var.hcloud_token}"
+  token = var.hcloud_id
 }
 
 data "hcloud_ssh_key" "ssh_key_1" {
@@ -9,96 +9,109 @@ data "hcloud_ssh_key" "ssh_key_1" {
 
 # Configure AWS provider
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region = "${var.region}"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.region
 }
 
 # Create new streaming server(s)
 resource "hcloud_server" "video" {
-    count = "${var.server_count}"
-    name = "${element(var.hostname, count.index)}${count.index + 1}-${element(var.location, count.index)}"
-    location = "${element(var.location, count.index)}"
-    image = "${var.image}"
-    server_type = "${var.server_type}"
-    ssh_keys = ["${data.hcloud_ssh_key.ssh_key_1.id}"]
+  count       = var.server_count
+  name        = "${element(var.hostname, count.index)}${count.index + 1}"
+  location    = element(var.location, count.index)
+  image       = var.image
+  server_type = var.server_type
+  ssh_keys    = [data.hcloud_ssh_key.ssh_key_1.id]
+}
+
+# Create reverse DNS
+
+resource "hcloud_rdns" "tradefox_rdns" {
+  count     = var.server_count
+  server_id = element(hcloud_server.video.*.id, count.index)
+  ip_address = element(hcloud_server.video.*.ipv4_address, count.index)
+  dns_ptr = var.domain
 }
 # Create multianswer A record(s) for server(s)
 resource "aws_route53_record" "tradefox" {
-  count = "${var.server_count}"
-  zone_id = "${var.dns_zone_id}"
-  name = "${var.domain}"
-  type    = "A"
-  ttl     = "${var.A_record_TTL}"
-  multivalue_answer_routing_policy  = "true"
+  count                            = var.server_count
+  zone_id                          = var.dns_zone_id
+  name                             = var.domain
+  type                             = "A"
+  ttl                              = var.A_record_TTL
+  multivalue_answer_routing_policy = "true"
 
-  set_identifier = "${element(var.hostname, count.index)}${count.index + 1}-${element(var.location, count.index)}"
-  records        = ["${element(hcloud_server.video.*.ipv4_address, count.index)}"]
-  health_check_id = "${element(aws_route53_health_check.health_check.*.id, count.index)}"
+  set_identifier  = "${element(var.hostname, count.index)}${count.index + 1}"
+  records         = [element(hcloud_server.video.*.ipv4_address, count.index)]
+  health_check_id = element(aws_route53_health_check.health_check.*.id, count.index)
 }
+
 # # Create multianswer AAAA record(s) for server(s)
 resource "aws_route53_record" "tradefoxv6" {
-  count = "${var.server_count}"
-  zone_id = "${var.dns_zone_id}"
-  name = "${var.domain}"
-  type    = "AAAA"
-  ttl     = "${var.A_record_TTL}"
-  multivalue_answer_routing_policy  = "true"
+  count                            = var.server_count
+  zone_id                          = var.dns_zone_id
+  name                             = var.domain
+  type                             = "AAAA"
+  ttl                              = var.A_record_TTL
+  multivalue_answer_routing_policy = "true"
 
-  set_identifier = "${element(var.hostname, count.index)}${count.index + 1}-${element(var.location, count.index)}V6"
-  records        = ["${element(hcloud_server.video.*.ipv6_address, count.index)}1"]
-  health_check_id = "${element(aws_route53_health_check.health_check.*.id, count.index)}"
+  set_identifier  = "${element(var.hostname, count.index)}${count.index + 1}V6"
+  records         = ["${element(hcloud_server.video.*.ipv6_address, count.index)}1"]
+  health_check_id = element(aws_route53_health_check.health_check.*.id, count.index)
 }
+
 # Create unique A record(s) for server(s)
 resource "aws_route53_record" "push" {
-  count = "${var.server_count}"
-  zone_id = "${var.dns_zone_id}"
-  name    = "${element(var.hostname, count.index)}${count.index + 1}-${element(var.location, count.index)}.${var.domain}."
+  count   = var.server_count
+  zone_id = var.dns_zone_id
+  name    = "${element(var.hostname, count.index)}${count.index + 1}.tradefox.biz."
   type    = "A"
-  ttl     = "${var.A_record_TTL}"
-  records = ["${element(hcloud_server.video.*.ipv4_address, count.index)}"]
+  ttl     = var.A_record_TTL
+  records = [element(hcloud_server.video.*.ipv4_address, count.index)]
 }
+
 # Create unique AAAA record(s) for server(s)
 resource "aws_route53_record" "pushv6" {
-  count = "${var.server_count}"
-  zone_id = "${var.dns_zone_id}"
-  name    = "${element(var.hostname, count.index)}${count.index + 1}-${element(var.location, count.index)}.${var.domain}."
+  count   = var.server_count
+  zone_id = var.dns_zone_id
+  name    = "${element(var.hostname, count.index)}${count.index + 1}.tradefox.biz."
   type    = "AAAA"
-  ttl     = "${var.A_record_TTL}"
+  ttl     = var.A_record_TTL
   records = ["${element(hcloud_server.video.*.ipv6_address, count.index)}1"]
 }
+
 # Create health check(s)
 resource "aws_route53_health_check" "health_check" {
-  count = "${var.server_count}"
-  ip_address        = "${element(hcloud_server.video.*.ipv4_address, count.index)}"
+  count             = var.server_count
+  ip_address        = element(hcloud_server.video.*.ipv4_address, count.index)
   port              = 80
   type              = "HTTP"
-  resource_path     = "/live/livestream.m3u8"
+  resource_path     = var.stream_path
   failure_threshold = "3"
   request_interval  = "10"
-
   tags = {
-    Name = "HealthCheck-${element(var.hostname, count.index)}${count.index + 1}-${element(var.location, count.index)}"
+    Name = "HealthCheck-${element(var.hostname, count.index)}${count.index + 1}"
   }
 }
+
 # Create alarm(s) for server(s)
 resource "aws_cloudwatch_metric_alarm" "alarm" {
-  count = "${var.server_count}"
-  alarm_name = "Alarm-${element(var.hostname, count.index)}${count.index + 1}-${element(var.location, count.index)}"
-  alarm_actions = ["your_sns_topic"]
-  comparison_operator = "LessThanThreshold"
-  ok_actions = []
-  evaluation_periods = "1"
-  metric_name = "HealthCheckStatus"
-  namespace = "AWS/Route53"
-  period = "60"
-  statistic = "Minimum"
-  threshold = "1.0"
-  evaluation_periods = "1"
-  alarm_description = "This metric monitors streaming server livestream"
+  count                     = var.server_count
+  alarm_name                = "Alarm-${element(var.hostname, count.index)}${count.index + 1}"
+  alarm_actions             = ["arn:aws:sns:us-east-1:991445153020:Nuremberg1_is_down"]
+  comparison_operator       = "LessThanThreshold"
+  ok_actions                = []
+  evaluation_periods        = "1"
+  metric_name               = "HealthCheckStatus"
+  namespace                 = "AWS/Route53"
+  period                    = "60"
+  statistic                 = "Minimum"
+  threshold                 = "1.0"
+  alarm_description         = "This metric monitors streaming server ${element(var.hostname, count.index)}${count.index + 1} livestream"
   insufficient_data_actions = []
-  actions_enabled = "true"
+  actions_enabled           = "true"
   dimensions = {
-      HealthCheckId = "${element(aws_route53_health_check.health_check.*.id, count.index)}"
-    }
+    HealthCheckId = element(aws_route53_health_check.health_check.*.id, count.index)
   }
+}
+
--- variables.tf
+++ variables.tf
@@ -1,20 +1,41 @@
-variable "hcloud_token" {}
-variable "image" {}
-variable "server_type" {}
-variable "region" {}
-variable "hostname" {default = 
-    ["test"]}
-variable "server_count" {}
+variable "hcloud_id" {
+}
+
+variable "image" {
+}
+
+variable "server_type" {
+}
+
+variable "region" {
+}
+
+variable "hostname" {
+  default = ["rtmp"]
+}
+
+variable "server_count" {
+}
+
 variable "location" {
-    default = 
-    ["nbg1", "fsn1"]
-    }
-variable "aws_access_key" {}
+  default = ["nbg1", "fsn1"]
+}
 
-variable "aws_secret_key" {}
+variable "aws_access_key" {
+}
 
-variable "A_record_TTL" {}
+variable "aws_secret_key" {
+}
 
-variable "domain" {}
+variable "A_record_TTL" {
+}
 
-variable "dns_zone_id" {}
+variable "domain" {
+}
+
+variable "dns_zone_id" {
+}
+
+variable "stream_path" {
+  
+}
